5c6d3eb5dcb4481300b22d55c249465e04c9c270
debangshuc <chakrabortydebangshu2@gmail.com>
2026-05-16 03:58:22 +0530

    Added Velocity updation logic

--- particle.c
+++ particle.c
@@ -3,11 +3,38 @@
 #define WIDTH 800
 #define HEIGHT 700
 
-
+// Particle Data
 typedef struct{
     float coord_x , coord_y, radius, vel_x , vel_y;
 }Particle;
 
+
+// Updates Particle Position on Screen
+void MoveParticle(Particle *particle){
+    particle->coord_x += particle->vel_x;
+    particle->coord_y += particle->vel_y;
+
+    float coord_x = particle->coord_x;
+    float coord_y = particle->coord_y;
+    float radius = particle->radius;
+
+    if (coord_x - radius <0){
+        particle->coord_x = -particle->coord_x;
+    }
+    if (coord_x + radius > WIDTH){
+        particle->coord_x = -particle->coord_x;
+    }
+    if (coord_y - radius < 0){
+        particle->coord_y = -particle->coord_y;
+    }
+    if (coord_y + radius > HEIGHT){
+        particle->coord_y = -particle->coord_y;
+    }
+
+}
+
+
+// Draws the Particle on Screen
 
 void ParticleDraw(Particle *particle){
     DrawCircle(particle->coord_x,particle->coord_y,
@@ -16,18 +43,20 @@ void ParticleDraw(Particle *particle){
 
 
 
+
+
 int main(void)
 {
     InitWindow(WIDTH, HEIGHT, "Particle Simulation");
     SetTargetFPS(60);
 
-    Particle particle = {300 , 300 , 35 , -5 , -5};
+    Particle particle = {250, 250 , 35 , -5 , -5};
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(GRAY);
-            
+            MoveParticle(&particle);
             ParticleDraw(&particle);
         EndDrawing();
     }
